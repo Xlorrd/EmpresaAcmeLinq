ba93479beacb1acad6a72a3eb4853651d6fb3751
Xlorrd <alx.simb@hotmail.com>
2019-11-12 23:18:43 -0500

    v.1.1
    Recuperacion de contraseña

--- CapaNegocio/UsuarioLogica.cs
+++ CapaNegocio/UsuarioLogica.cs
@@ -34,5 +34,39 @@ namespace CapaNegocio
             return auto;
         }
 
+        public void EnviarEmail(string EnviarE, string ContraseñaRecuperar)
+        {
+            //ahora accedemos a lo que hay en el web config que es el correo y contraseña de administrador
+            //string correoAdministrador = ConfigurationManager.AppSettings["CorreoElectronico"].ToString();
+            //string contraseñaAdministrador = ConfigurationManager.AppSettings["ContraseñaCorreo"].ToString();
+            string correoAdministrador = "alx.simb@gmail.com";
+            string contraseñaAdministrador = "";
+
+            string asunto = "Recuperacion de de Contraseña ";
+
+            string body = "Su Contraseña es: " + ContraseñaRecuperar;
+
+            //configuraciones smtp
+            var smtp = new SmtpClient();
+            {
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new NetworkCredential(correoAdministrador, contraseñaAdministrador);
+                smtp.Timeout = 20000; //tiempo de espera para mandar el correo 
+            }
+
+            try
+            {
+                smtp.Send(correoAdministrador, EnviarE, asunto, body);
+            }
+            catch (Exception ex)
+            {
+                //ScriptManager.RegisterClientScriptBlock(this.Page, this.GetType(), "AlertUsuario", "Window.onload - " +
+                //"function(){alert ('El usuario no existe');};", true);
+
+            }
+        }
     }
 }
